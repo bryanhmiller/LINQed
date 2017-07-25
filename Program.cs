da60374ac3175e5bdf6b3ceb2038f2851359c3ef
bryanhmiller <bryankalos@hotmail.com>
2017-07-24 19:24:43 -0500

    Finally completed editing out the extra programs and mains

--- Program.cs
+++ Program.cs
@@ -80,12 +80,22 @@ namespace LINQed
             };
 
             notPerfectSquares.TakeWhile(notPerfectSquare => notPerfectSquare % 1 != 0).ToList().ForEach(Console.WriteLine);
-       
+
+            /* 
+                Given the same customer set, display how many millionaires per bank.
+                Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+
+                Example Output:
+                WF 2
+                BOA 1
+                FTB 1
+                CITI 1
+            */
             customers
                 .Where(Customer => Customer.Balance > 999999.99)
                 .GroupBy(Customer => Customer.Bank).ToList()
                 .ForEach(x => Console.WriteLine($"{x.Key} has {x.Count()} millionaires!"));
-            
+
         }
 
 
@@ -110,88 +120,55 @@ namespace LINQed
                     new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
                 };
 
+
+        // ### Introduction to Joining Two Related Collections
+
+        // As a light introduction to working with relational databases, this example works with two collections of data - `banks` and `customers` - that are related through the `Bank` attribute on the customer. In that attribute, we store the abbreviation for a bank. However, we want to get the full name of the bank when we produce our output.
+
+        // This is called joining the collections together.
+
+        // This exercise is also an introduction to producing anonymous objects as the result of the LINQ statement.
+
+        // Read the [Group Join](https://code.msdn.microsoft.com/LINQ-Join-Operators-dabef4e9#groupjoin) example to get started.
+
+        /*
+            TASK:
+            As in the previous exercise, you're going to output the millionaires,
+            but you will also display the full name of the bank. You also need
+            to sort the millionaires' names, ascending by their LAST name.
+
+            Example output:
+                Tina Fey at Citibank
+                Joe Landy at Wells Fargo
+                Sarah Ng at First Tennessee
+                Les Paul at Wells Fargo
+                Peg Vale at Bank of America
+        */
+
+        // Define a bank
+
+        // Create some banks and store in a List
+        List<Bank> banks = new List<Bank>() {
+            new Bank(){ Name="First Tennessee", Symbol="FTB"},
+            new Bank(){ Name="Wells Fargo", Symbol="WF"},
+            new Bank(){ Name="Bank of America", Symbol="BOA"},
+            new Bank(){ Name="Citibank", Symbol="CITI"},
+
+        };
+
+        // var millionaireReport = ...
+
+        // foreach (var customer in millionaireReport)
+        //     {
+        //         Console.WriteLine($"{customer.Name} at {customer.Bank}");
+        //     }
     }
-    /* 
-        Given the same customer set, display how many millionaires per bank.
-        Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
 
-        Example Output:
-        WF 2
-        BOA 1
-        FTB 1
-        CITI 1
-    */
 
-    // ### Introduction to Joining Two Related Collections
-
-    // As a light introduction to working with relational databases, this example works with two collections of data - `banks` and `customers` - that are related through the `Bank` attribute on the customer. In that attribute, we store the abbreviation for a bank. However, we want to get the full name of the bank when we produce our output.
-
-    // This is called joining the collections together.
-
-    // This exercise is also an introduction to producing anonymous objects as the result of the LINQ statement.
-
-    // Read the [Group Join](https://code.msdn.microsoft.com/LINQ-Join-Operators-dabef4e9#groupjoin) example to get started.
-
-    /*
-        TASK:
-        As in the previous exercise, you're going to output the millionaires,
-        but you will also display the full name of the bank. You also need
-        to sort the millionaires' names, ascending by their LAST name.
-
-        Example output:
-            Tina Fey at Citibank
-            Joe Landy at Wells Fargo
-            Sarah Ng at First Tennessee
-            Les Paul at Wells Fargo
-            Peg Vale at Bank of America
-    */
-
-    // Define a bank
     public class Bank
     {
         public string Symbol { get; set; }
         public string Name { get; set; }
     }
 
-    // Define a customer
-    public class Customer
-    {
-        public string Name { get; set; }
-        public double Balance { get; set; }
-        public string Bank { get; set; }
-    }
-
-    public class Program
-    {
-        public static void Main()
-        {
-            // Create some banks and store in a List
-            List<Bank> banks = new List<Bank>() {
-            new Bank(){ Name="First Tennessee", Symbol="FTB"},
-            new Bank(){ Name="Wells Fargo", Symbol="WF"},
-            new Bank(){ Name="Bank of America", Symbol="BOA"},
-            new Bank(){ Name="Citibank", Symbol="CITI"},
-        };
-
-            // Create some customers and store in a List
-            List<Customer> customers = new List<Customer>() {
-            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
-            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
-            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
-            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
-            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
-            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
-            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
-            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
-            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
-            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
-        };
-
-            var millionaireReport = ...
-    
-        foreach (var customer in millionaireReport)
-            {
-                Console.WriteLine($"{customer.Name} at {customer.Bank}");
-            }
-        }
-    }
+}
