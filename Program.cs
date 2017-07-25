635022df612c210de7245c6355e71fb4ea1c8806
bryanhmiller <bryankalos@hotmail.com>
2017-07-24 19:05:25 -0500

    completed customer bank portion of assignment

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace LINQed
         {
             Console.WriteLine("Hello World!");
             // Find the words in the collection that start with the letter 'L'
-            List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"};
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
 
             var LFruits = from fruit in fruits where fruit.First() == 'L' select fruit;
             LFruits.ToList().ForEach(Console.WriteLine);
@@ -21,7 +21,7 @@ namespace LINQed
                 15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
             };
 
-            var fourSixMultiples = numbers.Where(number => number % 4 == 0 || number % 6 == 0 );
+            var fourSixMultiples = numbers.Where(number => number % 4 == 0 || number % 6 == 0);
             fourSixMultiples.ToList().ForEach(Console.WriteLine);
 
 
@@ -32,7 +32,7 @@ namespace LINQed
                 "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
                 "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
                 "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
-                "Francisco", "Tre" 
+                "Francisco", "Tre"
             };
 
             names.OrderByDescending(name => name).ToList().ForEach(Console.WriteLine);
@@ -68,109 +68,113 @@ namespace LINQed
 
             Console.WriteLine("The maximum price is ", prices.Max());
 
-// ### Partitioning Operations
+            /*
+                Store each number in the following List until a perfect square
+                is detected.
 
-/*
-    Store each number in the following List until a perfect square
-    is detected.
+                Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+            */
+            List<int> notPerfectSquares = new List<int>()
+            {
+                66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            };
 
-    Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
-*/
-List<int> wheresSquaredo = new List<int>()
-{
-    66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
-}
+            notPerfectSquares.TakeWhile(notPerfectSquare => notPerfectSquare % 1 != 0).ToList().ForEach(Console.WriteLine);
+       
+            customers
+                .Where(Customer => Customer.Balance > 999999.99)
+                .GroupBy(Customer => Customer.Bank).ToList()
+                .ForEach(x => Console.WriteLine($"{x.Key} has {x.Count()} millionaires!"));
+            
+        }
 
-// ### Using Custom Types
 
-// Build a collection of customers who are millionaires
-public class Customer
-{
-    public string Name { get; set; }
-    public double Balance { get; set; }
-    public string Bank { get; set; }
-}
+        // Build a collection of customers who are millionaires
+        public class Customer
+        {
+            public string Name { get; set; }
+            public double Balance { get; set; }
+            public string Bank { get; set; }
+        }
 
-public class Program
-{
-    public static void Main() {
-        List<Customer> customers = new List<Customer>() {
-            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
-            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
-            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
-            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
-            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
-            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
-            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
-            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
-            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
-            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
-        };
+        static List<Customer> customers = new List<Customer>() {
+                    new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                    new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                    new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                    new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                    new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                    new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                    new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                    new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                    new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                    new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+                };
+
     }
-}
-/* 
-    Given the same customer set, display how many millionaires per bank.
-    Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+    /* 
+        Given the same customer set, display how many millionaires per bank.
+        Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
 
-    Example Output:
-    WF 2
-    BOA 1
-    FTB 1
-    CITI 1
-*/
+        Example Output:
+        WF 2
+        BOA 1
+        FTB 1
+        CITI 1
+    */
 
-// ### Introduction to Joining Two Related Collections
+    // ### Introduction to Joining Two Related Collections
 
-// As a light introduction to working with relational databases, this example works with two collections of data - `banks` and `customers` - that are related through the `Bank` attribute on the customer. In that attribute, we store the abbreviation for a bank. However, we want to get the full name of the bank when we produce our output.
+    // As a light introduction to working with relational databases, this example works with two collections of data - `banks` and `customers` - that are related through the `Bank` attribute on the customer. In that attribute, we store the abbreviation for a bank. However, we want to get the full name of the bank when we produce our output.
 
-// This is called joining the collections together.
+    // This is called joining the collections together.
 
-// This exercise is also an introduction to producing anonymous objects as the result of the LINQ statement.
+    // This exercise is also an introduction to producing anonymous objects as the result of the LINQ statement.
 
-// Read the [Group Join](https://code.msdn.microsoft.com/LINQ-Join-Operators-dabef4e9#groupjoin) example to get started.
+    // Read the [Group Join](https://code.msdn.microsoft.com/LINQ-Join-Operators-dabef4e9#groupjoin) example to get started.
 
-/*
-    TASK:
-    As in the previous exercise, you're going to output the millionaires,
-    but you will also display the full name of the bank. You also need
-    to sort the millionaires' names, ascending by their LAST name.
+    /*
+        TASK:
+        As in the previous exercise, you're going to output the millionaires,
+        but you will also display the full name of the bank. You also need
+        to sort the millionaires' names, ascending by their LAST name.
 
-    Example output:
-        Tina Fey at Citibank
-        Joe Landy at Wells Fargo
-        Sarah Ng at First Tennessee
-        Les Paul at Wells Fargo
-        Peg Vale at Bank of America
-*/
+        Example output:
+            Tina Fey at Citibank
+            Joe Landy at Wells Fargo
+            Sarah Ng at First Tennessee
+            Les Paul at Wells Fargo
+            Peg Vale at Bank of America
+    */
 
-// Define a bank
-public class Bank
-{
-    public string Symbol { get; set; }
-    public string Name { get; set; }
-}
+    // Define a bank
+    public class Bank
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+    }
 
-// Define a customer
-public class Customer
-{
-    public string Name { get; set; }
-    public double Balance { get; set; }
-    public string Bank { get; set; }
-}
+    // Define a customer
+    public class Customer
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Bank { get; set; }
+    }
 
-public class Program
-{
-    public static void Main() {
-        // Create some banks and store in a List
-        List<Bank> banks = new List<Bank>() {
+    public class Program
+    {
+        public static void Main()
+        {
+            // Create some banks and store in a List
+            List<Bank> banks = new List<Bank>() {
             new Bank(){ Name="First Tennessee", Symbol="FTB"},
             new Bank(){ Name="Wells Fargo", Symbol="WF"},
             new Bank(){ Name="Bank of America", Symbol="BOA"},
             new Bank(){ Name="Citibank", Symbol="CITI"},
         };
 
-        // Create some customers and store in a List
-        List<Customer> customers = new List<Customer>() {
+            // Create some customers and store in a List
+            List<Customer> customers = new List<Customer>() {
             new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
             new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
             new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
@@ -183,11 +187,11 @@ public class Program
             new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
         };
 
-        var millionaireReport = ...
-
+            var millionaireReport = ...
+    
         foreach (var customer in millionaireReport)
-        {
-            Console.WriteLine($"{customer.Name} at {customer.Bank}");
+            {
+                Console.WriteLine($"{customer.Name} at {customer.Bank}");
+            }
         }
     }
-}
